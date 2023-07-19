8923a18ba7a6047a59aa633803a6d66d33cdfde8
Daineka A.U <zanoza193@mail.ru>
2023-07-19 20:42:13 +0300

    findByName implemented

--- src/main/java/Daineka/PhoneBook.java
+++ src/main/java/Daineka/PhoneBook.java
@@ -6,16 +6,17 @@ import java.util.List;
 public class PhoneBook {
     private List<String> contacts = new ArrayList<>();
 
-    public int add(String name, String number){
+    public int add(String name, String number) {
         String contact = name + ": " + number;
-        if (!contacts.contains(contact)){
+        if (!contacts.contains(contact)) {
             contacts.add(contact);
         }
         return contacts.size();
     }
-    public String findByNumber(String number){
-        for (String contact : contacts){
-            if(contact.endsWith(": " + number)){
+
+    public String findByNumber(String number) {
+        for (String contact : contacts) {
+            if (contact.endsWith(": " + number)) {
                 int index = contact.indexOf(":");
                 return contact.substring(0, index);
             }
@@ -23,10 +24,15 @@ public class PhoneBook {
         return null; // Если совпадения не найдено, возвращает `null`.
     }
 
-    public String findByName(String name){
-        return null;
+    public String findByName(String name) {
+        for (String contact : contacts) {
+            if (contact.startsWith(name)) {
+                int index = contact.indexOf(":");
+                if (index != -1) {
+                    return contact.substring(index + 2);
+                }
+            }
+        }
+        return null; // Если совпадения не найдено, возвращает `null`.
     }
-
-
-
 }

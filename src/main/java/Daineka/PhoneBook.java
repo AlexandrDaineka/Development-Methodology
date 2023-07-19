9d3698e4cc4c1a1cc2934b2edf5b48341e7680ee
Daineka A.U <zanoza193@mail.ru>
2023-07-19 19:27:12 +0300

    add implemented

--- src/main/java/Daineka/PhoneBook.java
+++ src/main/java/Daineka/PhoneBook.java
@@ -1,7 +1,16 @@
 package Daineka;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneBook {
-    public static int add(String name, String number){
-        return 0;
+    private List<String> contacts = new ArrayList<>();
+
+    public int add(String name, String number){
+        String contact = name + ": " + number;
+        if (!contacts.contains(contact)){
+            contacts.add(contact);
+        }
+        return contacts.size();
     }
 }

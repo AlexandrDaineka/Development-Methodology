c33f536b7d60fff775f8191cceebbfe063b5da46
Daineka A.U <zanoza193@mail.ru>
2023-07-19 19:15:07 +0300

    Stub and tests for add

--- src/main/java/Daineka/PhoneBook.java
+++ src/main/java/Daineka/PhoneBook.java
@@ -1,4 +1,7 @@
 package Daineka;
 
 public class PhoneBook {
+    public static int add(String name, String number){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import Daineka.PhoneBook;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int contactCount = phoneBook.add("John", "1234567890");
+        assertEquals(1, contactCount);
+    }
 }
